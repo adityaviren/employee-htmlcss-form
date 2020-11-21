1f86ec2b9c6d3dbe3d88ae8d6d57852305c4f1b6
Aditya Viren <adityaviren@gmail.com>
2020-11-21 15:23:08 +0530

    ADDED event listener in js and html

--- js/empform.js
+++ js/empform.js
@@ -63,3 +63,36 @@ class employeePayrollData{
     }
 
 }
+window.addEventListener('DOMContentLoaded',(event)=>{
+    const name=document.querySelector('#name');
+    const textError=document.querySelector('.text-error');
+    name.addEventListener('input',function(){
+        if(name.value.length==0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            textError.textContent = "Name is incorrect";
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+        const name=document.querySelector('#name');
+        const textError=document.querySelector('.text-error');
+        name.addEventListener('input',function(){
+            let nameRegex= RegExp('^[A-Z]{1}[a-z]{2,}$');
+            if(nameRegex.test(name.value))
+            textError.textContent = "";
+            else
+            textError.textContent = "Name is incorrect";
+        });
+
+    const salary = document.querySelector('#salary');
+    const output=document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function(){
+    output.textContent = salary.value;
+    });
+});
